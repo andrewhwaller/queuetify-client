90cf9ab19124fc525ca41c6c20ba392b1c708ce2
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2019-12-09 11:29:26 -0600

    Add runSearch, getSearchResults, and playPause functions

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ let homeVM = function() {
     self.playlists = ko.observableArray([]);
     self.webQueue = ko.observableArray([]);
     self.publicQueue = ko.observableArray([]);
+    self.searchBarInput = ko.observable()
 
 }
 
@@ -33,6 +34,29 @@ let getFriendList = function() {
 
 let addFriend = function() {
 
+}
+
+let runSearch = function() {
+    console.log(self.searchBarInput())
+    self.searchBarInput('')
+}
+
+let getSearchResults = function() {
+    
+}
+
+let playPause = function() {
+    let button = document.getElementById('playButton');
+    if (button.classList.contains('fa-pause')) {
+        button.classList.remove('fa-pause');
+        button.classList.add('fa-play');
+        button.classList.add('positive');
+        console.log('play')
+    } else if (button.classList.contains('fa-play'))  {
+        button.classList.add('fa-pause');
+        button.classList.remove('fa-play');
+        console.log('pause')
+    }
 }
 
 let vm = new homeVM();
